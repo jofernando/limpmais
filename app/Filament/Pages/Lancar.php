92423e22b2bcaa099cf98c2d72856f255bbf6c33
Jose Fernando Mendes da Costa <98fernandocosta@gmail.com>
2022-10-09 13:30:24 -0300

    utiliza repeater para fazer o lançamento das duplicatas

--- app/Filament/Pages/Lancar.php
+++ app/Filament/Pages/Lancar.php
@@ -6,8 +6,10 @@ use App\Models\Customer;
 use App\Models\Duplicata;
 use App\Rules\Lancamento;
 use Carbon\Carbon;
+use Closure;
 use Filament\Forms\Components\Grid;
 use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Wizard;
@@ -27,7 +29,7 @@ class Lancar extends Page
 
     public $texto;
 
-    public $duplicatas = [];
+    public $customers = [];
 
     public function mount(): void
     {
@@ -37,72 +39,47 @@ class Lancar extends Page
     protected function getFormSchema(): array
     {
         return [
-            Wizard::make([
-                Wizard\Step::make('Lançamentos')
+            Repeater::make('customers')
+                ->schema([
+                    Select::make('customer_id')
+                        ->required()
+                        ->reactive()
+                        ->label('Código/nome do customer')
+                        ->searchable()
+                        ->getSearchResultsUsing(fn (string $search) => Customer::where('nome', 'ilike', "%{$search}%")->orWhere('id', intval($search))->limit(10)->pluck('nome', 'id'))
+                        ->getOptionLabelUsing(fn ($value): ?string => Customer::find($value)?->nome)
+                        ->afterStateUpdated(function (Closure $set, $state) {
+                            $set('divida', Customer::find($state)?->divida);
+                        }),
+                    Grid::make()
                     ->schema([
-                        Textarea::make('texto')
-                            ->label('Duplicatas')
-                            ->rows(10)
-                            ->required()
-                            ->rule(new Lancamento())
-                            ->afterStateUpdated(fn() => $this->listarDuplicatas())
-                    ]),
-                Wizard\Step::make('Confirmação')
-                    ->schema([
-                        Repeater::make('duplicatas')
-                            ->schema([
-                                TextInput::make('nome')->disabled()->label('Cliente'),
-                                Grid::make()
-                                ->schema([
-                                    TextInput::make('codigo')->disabled()->label('Código'),
-                                    TextInput::make('pagar')->disabled()->label('Valor pago'),
-                                    TextInput::make('receber')->disabled()->label('Valor comprado'),
-                                    TextInput::make('restante')->disabled()->label('Dívida'),
-                                ])
-                                ->columns(4)
+                        TextInput::make('pago')->numeric()->minValue(0)->maxValue(fn (Closure $get) => $this->recuperarDivida($get('customer_id')))->label('Valor pago')->required(),
+                        TextInput::make('comprado')->numeric()->minValue(0)->label('Valor comprado')->required(),
+                        TextInput::make('divida')->numeric()->disabled()->label('Dívida'),
+                    ])
+                    ->columns(3)
 
-                            ])
-                            ->columns(1)
-                            ->disableItemCreation()
-                            ->disableItemDeletion()
-                            ->disableItemMovement()
-                    ]),
-            ])
-            ->submitAction(Action::make('submit')->button()->submit('form')->label('Enviar'))
+                ])
+                ->columns(1)
+                ->disableItemMovement(),
         ];
     }
 
-    public function listarDuplicatas()
+    private function recuperarDivida($customer_id)
     {
-        if($this->texto) {
-            $validator = Validator::make(['texto' => $this->texto], ['texto' => ['required', new Lancamento()]]);
-            if (!$validator->fails()) {
-                $this->duplicatas = [];
-                $exploded = explode(',', $this->texto);
-                $chunked = array_chunk($exploded, 3);
-                foreach ($chunked as $item) {
-                    $customer = Customer::find($item[0]);
-                    $divida = $customer->divida;
-                    if ($item[1] > 0) {
-                        $duplicata = ['codigo' => $item[0], 'nome' => $customer->identificacao, 'pagar' => $item[1], 'receber' => $item[2], 'restante' => $divida - $item[1] + $item[2]];
-                    } elseif ($item[1] < 0) {
-                        $duplicata = ['codigo' => $item[0], 'nome' => $customer->identificacao, 'pagar' => 0, 'receber' => $item[2], 'restante' => $divida + $item[2]];
-                    } else {
-                        $duplicata = ['codigo' => $item[0], 'nome' => $customer->identificacao, 'pagar' => $divida, 'receber' => $item[2], 'restante' => $item[2]];
-                    }
-                    $this->duplicatas[] = $duplicata;
-                }
-            }
+        if ($customer_id) {
+            return Customer::find($customer_id)?->divida;
         }
+        return 0;
     }
 
     public function submit()
     {
-        foreach ($this->duplicatas as $item) {
-            $customer = Customer::find($item['codigo']);
+        foreach ($this->customers as $item) {
+            $customer = Customer::find($item['customer_id']);
             $duplicatas = $customer->duplicatas()->where('quitada', false)->get();
-            $valorPago = floatval($item['pagar']);
-            $valorComprado = floatval($item['receber']);
+            $valorPago = floatval($item['pago']);
+            $valorComprado = floatval($item['comprado']);
             foreach ($duplicatas as $dupl) {
                 if ($valorPago >= $dupl->valor) {
                     $dupl->quitada = true;
@@ -122,5 +99,24 @@ class Lancar extends Page
         }
         $this->notify('success', 'Duplicatas lançadas com sucesso.');
         return $this->redirectRoute('filament.pages.lancar');
+    }
+
+    public function submitAndPrint()
+    {
+        $this->submit();
+        $duplicatas = array_map(fn($item) => $this->mapHelper($item), $this->customers);
+        $data = now()->format('d/m/Y');
+        $hora = now()->format('H:i:s');
+        $pdf = \PDF::loadView('impressao.duplicatas', compact('duplicatas', 'data', 'hora'))->output();
+        return response()->streamDownload( fn () => print($pdf), "duplicatas.pdf");
+    }
+
+    private function mapHelper($item)
+    {
+        $customer = Customer::find($item['customer_id']);
+        $item['codigo'] = $customer->id;
+        $item['nome'] = $customer->identificacao;
+        $item['data_vencimento'] = $customer->duplicatas()->where('quitada', false)->first()?->vencimento->format('d/m/Y');
+        return $item;
     }
 }
