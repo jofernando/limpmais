a74c5defc0c65ef4768f5781c5675ca3239fc23f
Jose Fernando Mendes da Costa <98fernandocosta@gmail.com>
2022-10-09 13:56:19 -0300

    utiliza repeater na página de imprimir

--- app/Filament/Pages/Imprimir.php
+++ app/Filament/Pages/Imprimir.php
@@ -3,21 +3,19 @@
 namespace App\Filament\Pages;
 
 use App\Models\Customer;
-use App\Rules\Impressao;
-use Filament\Forms\Components\Grid;
+use Closure;
 use Filament\Forms\Components\Repeater;
-use Filament\Forms\Components\Textarea;
-use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\Wizard;
-use Filament\Pages\Actions\Action;
+use Filament\Forms\Components\Select;
 use Filament\Pages\Page;
-use Illuminate\Support\Facades\Validator;
+use PDF;
 
 class Imprimir extends Page
 {
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
     protected static string $view = 'filament.pages.imprimir';
+
+    public $customers = [];
 
     public function mount(): void
     {
@@ -27,66 +25,40 @@ class Imprimir extends Page
     protected function getFormSchema(): array
     {
         return [
-            Wizard::make([
-                Wizard\Step::make('Lançamentos')
-                    ->schema([
-                        Textarea::make('texto')
-                            ->label('Duplicatas')
-                            ->rows(10)
-                            ->required()
-                            ->rule(new Impressao())
-                            ->afterStateUpdated(fn() => $this->listarDuplicatas())
-                    ]),
-                Wizard\Step::make('Confirmação')
-                    ->schema([
-                        Repeater::make('duplicatas')
-                            ->schema([
-                                TextInput::make('nome')->disabled()->label('Cliente'),
-                                Grid::make()
-                                    ->schema([
-                                        TextInput::make('codigo')->disabled()->label('Código'),
-                                        TextInput::make('divida')->disabled()->label('Dívida'),
-                                    ])
-                                    ->columns(2)
+            Repeater::make('customers')
+                ->schema([
+                    Select::make('customer_id')
+                        ->required()
+                        ->reactive()
+                        ->label('Código/nome do customer')
+                        ->searchable()
+                        ->getSearchResultsUsing(fn (string $search) => Customer::where('nome', 'ilike', "%{$search}%")->orWhere('id', intval($search))->limit(10)->pluck('nome', 'id'))
+                        ->getOptionLabelUsing(fn ($value): ?string => Customer::find($value)?->nome)
+                        ->afterStateUpdated(function (Closure $set, $state) {
+                            $set('divida', Customer::find($state)?->divida);
+                        }),
 
-                            ])
-                            ->columns(1)
-                            ->disableItemCreation()
-                            ->disableItemDeletion()
-                            ->disableItemMovement()
-                    ]),
-            ])
-            ->submitAction(Action::make('submit')->button()->submit('form')->label('Enviar'))
+                ])
+                ->columns(1)
+                ->disableItemMovement(),
         ];
-    }
-
-    public function listarDuplicatas()
-    {
-        if($this->texto) {
-            $validator = Validator::make(['texto' => $this->texto], ['texto' => ['required', new Impressao()]]);
-            if (!$validator->fails()) {
-                $this->duplicatas = [];
-                $exploded = explode(',', $this->texto);
-                $chunked = array_chunk($exploded, 3);
-                foreach ($chunked as $item) {
-                    $customer = Customer::find($item[0]);
-                    $divida = $customer->divida;
-                    if ($divida != 0) {
-                        $vencimento = $customer->duplicatas()->where('quitada', false)->first()->vencimento->format('d/m/Y');
-                        $duplicata = ['codigo' => $item[0], 'nome' => $customer->identificacao, 'divida' => $divida, 'data_vencimento' => $vencimento];
-                        $this->duplicatas[] = $duplicata;
-                    }
-                }
-            }
-        }
     }
 
     public function submit()
     {
+        $duplicatas = array_map(fn($item) => $this->mapHelper($item), $this->customers);
         $data = now()->format('d/m/Y');
         $hora = now()->format('H:i:s');
-        $duplicatas = $this->duplicatas;
-        $pdf = \PDF::loadView('impressao.duplicatas', compact('duplicatas', 'data', 'hora'))->output();
+        $pdf = PDF::loadView('impressao.duplicatas', compact('duplicatas', 'data', 'hora'))->output();
         return response()->streamDownload( fn () => print($pdf), "duplicatas.pdf");
+    }
+
+    private function mapHelper($item)
+    {
+        $customer = Customer::find($item['customer_id']);
+        $item['codigo'] = $customer->id;
+        $item['nome'] = $customer->identificacao;
+        $item['data_vencimento'] = $customer->duplicatas()->where('quitada', false)->first()?->vencimento->format('d/m/Y');
+        return $item;
     }
 }
--- resources/views/filament/pages/imprimir.blade.php
+++ resources/views/filament/pages/imprimir.blade.php
@@ -1,5 +1,8 @@
 <x-filament::page>
     <form action="#" wire:submit.prevent="submit" id="form">
         {{ $this->form }}
+        <button type="submit" class="inline-flex items-center justify-center py-1 gap-1 font-medium rounded-lg border transition-colors focus:outline-none focus:ring-offset-2 focus:ring-2 focus:ring-inset min-h-[2.25rem] px-4 text-sm text-white shadow focus:ring-white border-transparent bg-primary-600 hover:bg-primary-500 focus:bg-primary-700 focus:ring-offset-primary-700">
+            Enviar
+        </button>
     </form>
 </x-filament::page>
