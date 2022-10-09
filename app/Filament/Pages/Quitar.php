57acf9870dc1d30ca14f14d8617213987e5fa167
Jose Fernando Mendes da Costa <98fernandocosta@gmail.com>
2022-10-09 13:59:09 -0300

    utiliza repeater para quitar as dividas

--- app/Filament/Pages/Quitar.php
+++ app/Filament/Pages/Quitar.php
@@ -7,8 +7,10 @@ use App\Models\Duplicata;
 use App\Rules\CustomersCadastrados;
 use App\Rules\Lancamento;
 use Carbon\Carbon;
+use Closure;
 use Filament\Forms\Components\Grid;
 use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Wizard;
@@ -20,9 +22,8 @@ class Quitar extends Page
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
     protected static string $view = 'filament.pages.quitar';
-    public $texto;
 
-    public $duplicatas = [];
+    public $customers = [];
 
     public function mount(): void
     {
@@ -32,55 +33,28 @@ class Quitar extends Page
     protected function getFormSchema(): array
     {
         return [
-            Wizard::make([
-                Wizard\Step::make('Quitar dívidas')
-                    ->schema([
-                        Textarea::make('texto')
-                            ->label('Customers')
-                            ->rows(10)
-                            ->required()
-                            ->rule(new CustomersCadastrados())
-                            ->afterStateUpdated(fn() => $this->listarDuplicatas())
-                    ]),
-                Wizard\Step::make('Confirmação')
-                    ->schema([
-                        Repeater::make('duplicatas')
-                            ->schema([
-                                TextInput::make('nome')->disabled()->label('Cliente'),
-                                Grid::make()
-                                    ->schema([
-                                        TextInput::make('codigo')->disabled()->label('Código'),
-                                        TextInput::make('divida')->disabled()->label('Dívida'),
-                                    ])
-                                    ->columns(2)
-
-                            ])
-                            ->columns(1)
-                            ->disableItemCreation()
-                            ->disableItemDeletion()
-                            ->disableItemMovement()
-                    ]),
-            ])
-            ->submitAction(Action::make('submit')->button()->submit('form')->label('Enviar'))
+            Repeater::make('customers')
+                ->schema([
+                    Select::make('customer_id')
+                        ->required()
+                        ->reactive()
+                        ->label('Código/nome do customer')
+                        ->searchable()
+                        ->getSearchResultsUsing(fn (string $search) => Customer::where('nome', 'ilike', "%{$search}%")->orWhere('id', intval($search))->limit(10)->pluck('nome', 'id'))
+                        ->getOptionLabelUsing(fn ($value): ?string => Customer::find($value)?->nome)
+                        ->afterStateUpdated(function (Closure $set, $state) {
+                            $set('divida', Customer::find($state)?->divida);
+                        }),
+                    TextInput::make('divida')->numeric()->disabled()->label('Dívida'),
+                ])
+                ->columns(1)
+                ->disableItemMovement(),
         ];
-    }
-
-    public function listarDuplicatas()
-    {
-        if($this->texto) {
-            $validator = Validator::make(['texto' => $this->texto], ['texto' => ['required', new CustomersCadastrados()]]);
-            if (!$validator->fails()) {
-                $this->duplicatas = [];
-                $exploded = explode(',', $this->texto);
-                $customers = Customer::whereIn('id', $exploded)->get();
-                $this->duplicatas = $customers->map(fn ($customer) => ['codigo' => $customer->id, 'nome' => $customer->identificacao, 'divida' => $customer->divida])->all();
-            }
-        }
     }
 
     public function submit()
     {
-        Duplicata::whereIn('customer_id', array_column($this->duplicatas, 'codigo'))->where('quitada', false)->update(['quitada' => true]);
+        Duplicata::whereIn('customer_id', array_column($this->duplicatas, 'customer_id'))->where('quitada', false)->update(['quitada' => true]);
         $this->notify('success', 'Duplicatas quitadas com sucesso.');
         return $this->redirectRoute('filament.pages.quitar');
     }
