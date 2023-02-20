071da4035b8e459339c1824b17fce27f48604108
Jose Fernando Mendes da Costa <98fernandocosta@gmail.com>
2023-02-20 15:43:57 -0300

    remover caracter errado

--- app/Filament/Pages/Lancar.php
+++ app/Filament/Pages/Lancar.php
@@ -35,7 +35,7 @@ class Lancar extends Page
             'nome' => '',
             'divida' => '',
             'pago' => '',
-            'comprado' => ','
+            'comprado' => '',
         ],
     ];
 
