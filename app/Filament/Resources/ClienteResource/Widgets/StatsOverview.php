fc6273c031c2b1870d347a24fdaa3c8bfa90d642
José Fernando Mendes da Costa <98fernandocosta@gmail.com>
2024-03-01 16:05:30 -0300

    atualiza stats no dashboard

--- app/Filament/Resources/ClienteResource/Widgets/StatsOverview.php
+++ app/Filament/Resources/ClienteResource/Widgets/StatsOverview.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\ClienteResource\Widgets;
 
 use App\Models\Cliente;
 use App\Models\Duplicata;
+use App\Models\Pagamento;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Card;
 
@@ -12,15 +13,19 @@ class StatsOverview extends BaseWidget
     protected function getCards(): array
     {
         $mes = [now()->subDays(30), now()];
-        $recebidos = Duplicata::whereBetween('pagamento', $mes)->sum('pago');
-        $duplicatas = Duplicata::whereBetween('created_at', $mes)->whereNotNull('compra')->get();
+        $recebidos = Pagamento::whereBetween('data', $mes)->sum('valor');
+        $duplicatas = Duplicata::whereBetween('created_at', $mes)->get();
+        $vendas = $duplicatas->sum('valor');
         $compras = $duplicatas->sum('compras');
-        $compras = $duplicatas->sum('gastos');
-        $lucro = $duplicatas->sum('valor') - $compras;
+        $gastos = $duplicatas->sum('gastos');
+        $lucro = $vendas - $compras - $gastos;
         return [
             Card::make('Clientes cadastrados', Cliente::count()),
             Card::make('Duplicatas recebidas nos ultimos 30 dias', "R$ " . number_format($recebidos, 2, ',', '.')),
-            Card::make('Lucro nos ultimos 30 dias', "R$ " . number_format($lucro, 2, ',', '.')),
+            Card::make('Compras das duplicatas nos ultimos 30 dias', 'R$'. number_format($compras, 2, ',', '.')),
+            Card::make('Vendas das duplicatas nos ultimos 30 dias', 'R$'. number_format($vendas, 2, ',', '.')),
+            Card::make('Gastos das duplicatas nos ultimos 30 dias', 'R$'. number_format($gastos, 2, ',', '.')),
+            Card::make('Lucro estimado nos ultimos 30 dias', "R$ " . number_format($lucro, 2, ',', '.')),
         ];
     }
 }
