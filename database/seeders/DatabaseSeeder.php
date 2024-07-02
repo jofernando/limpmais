6f375d775cb7e6ea5e0acce07ff9974b54c47a98
José Fernando Mendes da Costa <98fernandocosta@gmail.com>
2024-07-02 20:46:41 -0300

    test: utiliza o recycle

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,9 +4,13 @@ namespace Database\Seeders;
 
 use App\Models\Cliente;
 use App\Models\Duplicata;
+use App\Models\Fornecedor;
 use App\Models\MetodoPagamento;
+use App\Models\Motorista;
 use App\Models\Pagamento;
+use App\Models\Produto;
 use App\Models\User;
+use App\Models\Veiculo;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -19,18 +23,30 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $credcards = MetodoPagamento::factory()->count(2)->create();
+        $fornecedores = Fornecedor::factory()->count(5)->create();
+        $motoristas = Motorista::factory()->count(5)->create();
+        $veiculos = Veiculo::factory()->count(5)->create();
+        $produtos = Produto::factory()->count(10)->create();
         Cliente::factory()->has(
-            Duplicata::factory()->count(3)->has(
-                Pagamento::factory()->count(2)->state(
-                    function (array $attributes, Duplicata $duplicata) use ($credcards) {
-                        return [
-                            'valor' => $duplicata->valor / 3,
-                            'metodo_pagamento_id' => $credcards->random(1)->first()->id,
-                        ];
-                    }
+            Duplicata::factory()
+                ->count(3)
+                ->recycle($fornecedores)
+                ->recycle($motoristas)
+                ->recycle($veiculos)
+                ->recycle($produtos)
+                ->has(
+                    Pagamento::factory()->count(2)
+                        ->recycle($credcards)
+                        ->state(
+                            function (array $attributes, Duplicata $duplicata) {
+                                return [
+                                    'valor' => $duplicata->valor / 3,
+                                ];
+                            }
+                        )
                 )
             )
-        )->count(30)->create();
+            ->count(30)->create();
         Cliente::factory()->count(8)->create();
         User::factory()->create();
     }
